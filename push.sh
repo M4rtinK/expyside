28829e973b066b3111f01322acde45f9dc48df0c
Martin Kolman <martin.kolman@gmail.com>
2013-02-17 23:23:05 +0100

    update the easy push script

--- push.sh
+++ push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-adb push main.py /sdcard/pyqtlauncher/main.py
-adb push main.qml /sdcard/pyqtlauncher/main.qml
-adb push pyside.png /sdcard/pyqtlauncher/pyside.png
-
-
+#adb push main.py /sdcard/pyqtlauncher/main.py
+adb push main.py /data/data/org.modrana.PySideExample/files/main.py
+adb push main.qml /data/data/org.modrana.PySideExample/files/main.qml
+adb push pyside.png /data/data/org.modrana.PySideExample/files/pyside.png
